548d150c004b3411b12835bc640db78ab198fc6e
Coffee☕️ <coffee@coffeebot.dev>
2024-08-16 19:15:59 -0400

    use NEXT_PUBLIC_RPC_URL as default if available

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
-import {cookieStorage, createStorage} from "wagmi";
-import {mainnet, sepolia, anvil} from "viem/chains";
-import {defaultWagmiConfig} from "@web3modal/wagmi";
+import { mainnet } from "viem/chains";
+import { defaultWagmiConfig } from "@web3modal/wagmi";
+import { fallback, http } from "viem";
 
 export const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_ID;
 export const defaultChain = mainnet;
@@ -14,9 +14,26 @@ const metadata = {
   icons: ["https://dyadstable.xyz/favicon-32x32.png"],
 };
 
+
+let defaultTransport;
+if (process.env.NEXT_PUBLIC_RPC_URL) {
+  defaultTransport = fallback(
+    [
+      http(process.env.NEXT_PUBLIC_RPC_URL),
+      http()
+    ]
+  )
+}
+else {
+  defaultTransport = http();
+}
+
 const chains = [defaultChain] as const;
 export const wagmiConfig = defaultWagmiConfig({
   chains,
   projectId,
   metadata,
+  transports: {
+    [mainnet.id]: defaultTransport
+  },
 });
