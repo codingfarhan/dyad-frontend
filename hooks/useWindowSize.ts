2f3edf06262bbff1aa88467e649b542994a674bc
Mostafa Mahmoud <mostafamahmoud24@gmail.com>
2024-08-19 03:53:25 +0300

    fixed an issue where window was incorrectly referenced

--- hooks/useWindowSize.ts
+++ hooks/useWindowSize.ts
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
 const useWindowSize = () => {
   const getWindowDimensions = () => {
-    const { innerHeight: windowHeight, innerWidth: windowWidth } = window;
+    if (window) {
+      const { innerHeight: windowHeight, innerWidth: windowWidth } = window;
+      return {
+        windowHeight,
+        windowWidth,
+      };
+    }
+
     return {
-      windowHeight,
-      windowWidth,
+      windowHeight: 0,
+      windowWidth: 0,
     };
   };
   const [windowSize, setWindowSize] = useState(getWindowDimensions());
