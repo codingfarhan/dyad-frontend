bacebe016aafd91ae4ec2b5ee11a8c3adc8e5c67
shafu.eth <selfouly@gmail.com>
2024-08-22 15:39:13 +0300

    add vault decimals

--- lib/constants.ts
+++ lib/constants.ts
@@ -20,30 +20,36 @@ export const vaultInfo = [
     vaultAddress: wEthVaultAddress[defaultChain.id],
     symbol: "wETH",
     tokenAddress: wethAddress[defaultChain.id],
+    decimals: 8,
   },
   {
     vaultAddress: wstEthVaultAddress[defaultChain.id],
     symbol: "wstETH",
     tokenAddress: wstEthAddress[defaultChain.id],
+    decimals: 8,
   },
   {
     vaultAddress: keroseneVaultV2Address[defaultChain.id],
     symbol: "KEROSENE",
     tokenAddress: keroseneAddress[defaultChain.id],
+    decimals: 8,
   },
   {
     vaultAddress: tBtcVaultAddress[defaultChain.id],
     symbol: "tBTC",
     tokenAddress: tBtcAddress[defaultChain.id],
+    decimals: 8,
   },
   {
     vaultAddress: sUsDeVaultAddress[defaultChain.id],
     symbol: "sUSDe",
     tokenAddress: sUsDeAddress[defaultChain.id],
+    decimals: 8,
   },
   {
     vaultAddress: weEthVaultAddress[defaultChain.id],
     symbol: "weETH",
     tokenAddress: weEthAddress[defaultChain.id],
+    decimals: 18,
   },
 ];
