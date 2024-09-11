e3bbd7f6297609407a775ea5c1c98c81af98191c
Coffee☕️ <coffee@coffeebot.dev>
2024-09-10 21:36:48 -0400

    safe app

--- next.config.js
+++ next.config.js
@@ -12,7 +12,26 @@ const nextConfig = {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
   },
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  async headers() {
+    return [{
+      source: '/manifest.json',
+      headers: [
+        {
+          key: 'Access-Control-Allow-Origin',
+          value: '*'
+        },
+        {
+          key: 'Access-Control-Allow-Methods',
+          value: 'GET'
+        },
+        {
+          key: 'Access-Control-Allow-Headers',
+          value: 'X-Requested-With, content-type, Authorization'
+        }
+      ]
+    }]
+  }
 };
 
 module.exports = nextConfig;
